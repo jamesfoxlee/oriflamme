18b2934bae72b4d25050eadab89fa9c91eb773f2
James Foxlee <james.foxlee@protonmail.com>
2021-11-02 10:49:15 +0000

    chore: resolve conflicts .gitignore and package.json

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,14 +1,21 @@
 import io from 'socket.io-client';
 
 export default function Socket() {
-  console.log('initialising socket manager');
-  const socket = io('http://localhost:19126');
 
+  console.log('Socket.init() initialising socket...');
+  const socket = io('http://localhost:19126');
   socket.on('connect', () => {
     console.log(`connected to server, socket.id: ${socket.id}`);
   });
-
   socket.on('disconnect', () => {
     console.log(`disconnected from server, socket.id (should be undefined): ${socket.id}`);
   });
+
+  const sendMessage = (message) => {
+    socket.emit('message')
+  }
+
+  return {
+    sendMessage,
+  }
 }
