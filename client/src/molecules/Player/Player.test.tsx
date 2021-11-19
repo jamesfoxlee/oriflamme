5233f5b912ac9f4bd836f0a0bf2e02dc27b9ba9a
Eloy <eloy.robillard@gmail.com>
2021-11-19 15:01:21 +0100

    🧪 test(player molecule): top div class name

--- client/src/molecules/Player/Player.test.tsx
+++ client/src/molecules/Player/Player.test.tsx
@@ -1,35 +1,39 @@
 import Player from './Player';
-import { Card, PlayerType } from '../../types';
-import { cards } from '../../mocks/cards.mocks';
 import { Props } from './Player';
 import '@testing-library/jest-dom';
 
 import React from 'react';
 import { act } from 'react-dom/test-utils';
-import { render, unmountComponentAtNode } from 'react-dom';
+import { render } from '@testing-library/react'
 
-describe('Player', () => {
-
-  let props: Props;
-  beforeEach(() => {
-    props = {
-      isActivePlayer: false,
-      // player: {
-      //   color: 'red',
-      //   discardPile: string[];
-      //   hand: string[];
-      //   id: string;
-      //   imageUrl: string;
-      //   influence: number;
-      //   name: string;
-      //   roomId: string;
-      // },
+let props: Props;
+beforeEach(() => {
+  props = {
+    isActivePlayer: false,
+    player: {
+      color: 'red',
+      discardPile: [],
+      hand: [
+        'archer',
+        'assassination',
+        'conspiracy'
+      ],
+      id: 'n5EKpbcQWdoDvn8lAAAF',
+      imageUrl:
+        'https://i.pinimg.com/originals/7a/ae/c8/7aaec855ca19276f3884f6b7655fdc33.jpg',
+      influence: 1,
+      name: 'JFK',
+      roomId: '3b805350-4897-11ec-af1c-139452ba1c4c'
     }
-  });
+  };
+});
 
-  describe('top div', () => {
-    it('should have the correct class name based on player activity', () => {
+test('top div should have the correct class name based on player activity', () => {
+  const { container } = render(<Player {...props}/>);
+  expect(container.querySelector('.player')).toBeInTheDocument();
+  expect(container.querySelector('.player--active')).toBe(null);
 
-    });
-  });
+  const { container: container2 } = render(<Player {...{...props, isActivePlayer: true}}/>);
+  expect(container2.querySelector('.player')).toBeInTheDocument();
+  expect(container2.querySelector('.player--active')).toBeInTheDocument();
 });
