87c6a657cfdbe04e147ae1b6e869a2443e7af6db
natas <natasa410@hotmail.com>
2021-11-24 11:24:11 +0000

    attempt to fix test

--- client/src/mocks/qcards.mocks.ts
+++ client/src/mocks/qcards.mocks.ts
@@ -1,34 +1,33 @@
-import { QCard } from '../types/index';
+import { QCard } from "../types/index";
 
 type QCardMocks = {
-	[key: string]: QCard;
+  [key: string]: QCard;
 };
 
 export const qCardMocks: QCardMocks = {
-	placeholder: {
-		id: 'placeholder',
-		name: 'Placeholder',
-		text: 'Just a fake card to use as default.',
-		influence: 0,
-		ownerId: '',
-		ownerColor: ''
-	},
-	simple: {
-		id: 'conspiracy',
-		name: 'Conspiracy',
-		text:
-			'Gain double the influence accumulated on Conspiracy when it is revealed. Discard Conspiracy.',
-		influence: 0,
-		ownerId: '',
-		ownerColor: 'red'
-	},
-	hidden: {
-		id: 'assassination',
-		name: 'Assassination',
-		text: 'Eliminate any card in the Queue. Discard Assassination.',
-		revealed: false,
+  placeholder: {
+    id: "placeholder",
+    name: "Placeholder",
+    text: "Just a fake card to use as default.",
     influence: 0,
-    ownerId: '',
-    ownerColor: 'red'
-	}
+    ownerId: "",
+    ownerColor: "",
+  },
+  simple: {
+    id: "conspiracy",
+    name: "Conspiracy",
+    text: "Gain double the influence accumulated on Conspiracy when it is revealed. Discard Conspiracy.",
+    influence: 0,
+    ownerId: "",
+    ownerColor: "red",
+  },
+  hidden: {
+    id: "assassination",
+    name: "Assassination",
+    text: "Eliminate any card in the Queue. Discard Assassination.",
+    revealed: false,
+    influence: 0,
+    ownerId: "",
+    ownerColor: "red",
+  },
 };
--- client/src/views/Game/SideBar.tsx
+++ client/src/views/Game/SideBar.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import { User, GameState, Card, MessageType } from '../../types/index';
+import React from "react";
+import { User, GameState, Card, MessageType } from "../../types/index";
 import Messages from "../../organisms/Messages/Messages";
 import Status from "../../atoms/Status/Status";
 
 type Props = {
-  gameState: GameState,
-  messages: MessageType[],
-  selectedPlayerCard: Card,
-  user: User
-}
+  gameState: GameState;
+  messages: MessageType[];
+  selectedPlayerCard: Card;
+  user: User;
+};
 
-export default function SideBar ({gameState, messages, selectedPlayerCard, user}: Props) {
-	return (
-		<div className='game__sidebar'>
-			<div className='game__status'>
-				<Status
-					gameState={gameState}
-					selectedPlayerCard={selectedPlayerCard}
-					user={user}
-				/>
-			</div>
-			<div className='game__messages'>
-				<Messages
-					messages={messages}
-					players={gameState.players}
-				/>
-			</div>
-		</div>
-	);
+export default function SideBar({
+  gameState,
+  messages,
+  selectedPlayerCard,
+  user,
+}: Props) {
+  return (
+    <div className="game__sidebar">
+      <div className="game__status">
+        <Status
+          gameState={gameState}
+          selectedPlayerCard={selectedPlayerCard}
+          user={user}
+        />
+      </div>
+      <div className="game__messages">
+        <Messages messages={messages} players={gameState.players} />
+      </div>
+    </div>
+  );
 }
